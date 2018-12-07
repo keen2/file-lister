a94523e1ff79059cf9aba9259e2a4c337131a41b
Andrei Ermishin <andrey.yermishin@gmail.com>
2018-12-07 23:14:23 +0300

    Add cmd options and scan_directory()
    If App was run with cmd options it can scan without GUI. scan_directory returns 1 level list of files/subdirectories.

--- file-lister.py
+++ file-lister.py
@@ -14,16 +14,39 @@ __email__ = "andrey.yermishin@gmail.com"
 __status__ = "Prototype"
 
 
+usage = '\nNote, usage: %s dir_path file_path\n'
+usage += 'Shortly for current directory: %s . file_name\n'
+
+
 import tkinter as tk
 # Explicitly import some submodules:
 from tkinter import ttk
 from tkinter import filedialog
 
-import os
+# For low-level path manipulation on strings: import os.
+# New module offers classes representing filesystem paths.
+from pathlib import Path
+
+
+def scan_directory(path):
+    '''
+    Returns a list of files and directories along given path.
+    ######## desc
+    '''
+    entries_list = []
+    # Filter
+    # for entry in pathlib_path.glob('*.py'):
+    for entry in path.iterdir():
+        # if entry.is_dir():
+        entries_list.append(entry.name)
+
+    return entries_list
 
 
 class Application(tk.Frame):
-    '''Class is used to make structured text of files list at directories.'''
+    '''
+    Class is used to create GUI for scanning files in directories.
+    '''
 
     def __init__(self, master=None):
         '''Constructor for Application.'''
@@ -32,12 +55,36 @@ class Application(tk.Frame):
 
 
 def main(argv):
-    # Create a toplevel widget of tkinter as main window of an application
-    app = Application()
-    # Set title of master frame
-    app.master.title("FileLister")
-    # Run the main loop of Tcl.
-    app.mainloop()
+
+    # Run GUI.
+    if len(argv) == 1:
+        # Create a toplevel widget of tkinter as main window of an application
+        app = Application()
+        app.master.title('FileLister ' + __version__[:-2])
+        app.master.geometry('640x480')
+        app.master.resizable(False, False)
+        # Run the main loop of Tcl.
+        app.mainloop()
+    
+    # Use console.
+    elif len(argv) > 1:
+        if Path(argv[1]).is_dir():
+            # Path object is returned.
+            dir_path = Path(argv[1])
+            found_files = scan_directory(dir_path)
+
+            # Print results to file.
+            if len(argv) > 2:
+                file_path = Path(argv[2])###
+                with file_path.open('w') as fhand:
+                    print(*found_files, sep='\n', file=fhand)
+            
+            # if no file_path print to console.
+            else:
+                print(*found_files, sep='\n')
+
+        else:
+            print('Invalid directory path!', usage.replace('%s', argv[0]))
 
 
 if __name__ == "__main__":
@@ -48,5 +95,6 @@ if __name__ == "__main__":
 
 
 # TODO:
-# -if posiible do it recursively
-# -argv[0]=self, argv[1]=source/dir, argv[2]=dest/file
+# - if file_path doesn't have ".txt" (suffix)
+# - if posiible do scan recursively
+# - maxdepth
