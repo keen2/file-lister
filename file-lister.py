0201bf7d46a84706b1b650e42d538533b737fa65
Andrei Ermishin <andrey.yermishin@gmail.com>
2018-12-27 22:34:55 +0300

    Add and pack (initially) widgets to main window.
    Add Labelframes plus labels and buttons, Saveas dialog. Packing is basic now. Style and font will be default. PEP8 corrections.

--- file-lister.py
+++ file-lister.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-'''
-Script scans for files and subdirectories in a directory and writes to file 
+"""Script scans for files and subdirectories in a directory and writes to file 
 a list of data structured by hierarchy.
-'''
+Requires Python 3.4 for pathlib module.
+"""
 
 __author__ = "Andrei Ermishin"
 __copyright__ = "Copyright 2018"
 __credits__ = []
 __license__ = "MIT"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "Andrei Ermishin"
 __email__ = "andrey.yermishin@gmail.com"
 __status__ = "Prototype"
@@ -27,13 +27,40 @@ from tkinter import filedialog
 # New module offers classes representing filesystem paths.
 from pathlib import Path
 
+from datetime import date
+
+# 16 x 9
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 450
+
+DEF_FNAME = 'FileLister'
+
 
 def scan_directory(path):
-    '''
+    """Input: Path object of directory.
+    ######## desc
     Returns a list of files and directories along given path.
     ######## desc
-    '''
+    """
+    # def tree(directory):
+    #     print(f'+ {directory}')
+    #     for path in sorted(directory.rglob('*')):
+    #         depth = len(path.relative_to(directory).parts)
+    #         spacer = '    ' * depth
+    #         print(f'{spacer}+ {path.name}')
+    # Output:
+    # >>> tree(pathlib.Path.cwd())
+    # + /home/gahjelle/realpython
+    #     + directory_1
+    #         + file_a.md
+    #     + directory_2
+    #         + file_a.md
+    #         + file_b.pdf
+    #         + file_c.py
+    #     + file_1.txt
+    #     + file_2.txt
     entries_list = []
+    # recursive - rglob()
     # Filter
     # for entry in pathlib_path.glob('*.py'):
     for entry in path.iterdir():
@@ -43,28 +70,102 @@ def scan_directory(path):
     return entries_list
 
 
-class Application(tk.Frame):
-    '''
-    Class is used to create GUI for scanning files in directories.
-    '''
+class Window(ttk.Frame):
+    """Class is used to create GUI for scanning files in directories."""
 
     def __init__(self, master=None):
-        '''Constructor for Application.'''
-        # make the application actually appear on the screen
+        """Construct a Ttk Frame."""
+        # The class instance (=self) will be content=ttk.Frame(root)
         super().__init__(master)
+        self.master = master    # it's root=tk.TK()
+        self.dir_path = Path.cwd()
+        self.file_path = self.dir_path.joinpath(DEF_FNAME 
+                                                + str(date.today()) + '.txt')
+        self.frame_padx = 15
+        self.frame_pady = 10
+        
+        self.create_widgets()
+        self.arrange_widgets()
+    
+    def create_widgets(self):
+        """Create widgets within main frame (self)."""
+        # dir_frame
+        self.dir_frame = ttk.Labelframe(self.master, text='Directory to scan')
+        self.choose_dir_btn = ttk.Button(self.dir_frame, text='Choose...',
+                                    command=self.master.destroy)
+        self.dir_lbl = ttk.Label(self.dir_frame)
+        self.dir_lbl_text = tk.StringVar()
+        self.dir_lbl['textvariable'] = self.dir_lbl_text
+        self.dir_lbl_text.set(str(self.dir_path))
+
+        # file_frame
+        self.file_frame = ttk.Labelframe(self.master, text='File to save')
+        self.save_file_btn = ttk.Button(self.file_frame, text='Save as...',
+                                    command=self.save_file_dlg)
+        self.file_lbl = ttk.Label(self.file_frame)
+        self.file_lbl_text = tk.StringVar()
+        self.file_lbl['textvariable'] = self.file_lbl_text
+        self.file_lbl_text.set(str(self.file_path))
+
+        # options_frame
+        self.options_frame = ttk.Labelframe(self.master, text='Options')
+        self.scan_subfolders = tk.BooleanVar()
+        self.scan_subfolders.set(True)
+        self.scan_subf_chk = ttk.Checkbutton(self.options_frame, 
+                                text='Scan subfolders', 
+                                variable=self.scan_subfolders, onvalue=True)
+
+        self.run_btn = ttk.Button(self.master, text='Run',
+                                    command=self.master.destroy)
+        self.about_btn = ttk.Button(self.master, text='About',
+                                    command=self.master.destroy)
+        self.quit_btn = ttk.Button(self.master, text='Exit',
+                                    command=self.master.destroy)
+
+    def arrange_widgets(self):
+        """Organaze and show widgets."""
+
+        self.dir_frame.pack(fill='x', 
+                            padx=self.frame_padx, pady=self.frame_pady)
+        self.choose_dir_btn.pack(side='left', padx=20, pady=20)
+        self.dir_lbl.pack(side='right', padx=20, pady=20)
+
+        self.file_frame.pack(fill='x', 
+                            padx=self.frame_padx, pady=self.frame_pady)
+        self.save_file_btn.pack(side='left', padx=20, pady=20)
+        self.file_lbl.pack(side='right', padx=20, pady=20)
+
+        self.options_frame.pack(fill='x',
+                                padx=self.frame_padx, pady=self.frame_pady)
+        self.scan_subf_chk.pack()
+
+        self.run_btn.pack(side='right', padx=20, pady=20)
+        self.about_btn.pack(side='left', padx=20, pady=20)
+        self.quit_btn.pack(side='right', padx=20, pady=20)
+    
+    def save_file_dlg(self):
+        """Open dialog window which allows to choose a path for saving."""
+
+        fname = filedialog.asksaveasfilename(defaultextension='.txt', 
+                    filetypes=[('All files', '.*'), ('Text files', '.txt')], 
+                    initialfile=DEF_FNAME + str(date.today()))
+        if fname:
+            self.file_path = Path(fname)
+            self.file_lbl_text.set(fname)
 
 
 def main(argv):
-
+    """Uses GUI class or console to scan folders depending on arguments."""
     # Run GUI.
     if len(argv) == 1:
         # Create a toplevel widget of tkinter as main window of an application
-        app = Application()
+        root = tk.Tk()
+        app = Window(root)
         app.master.title('FileLister ' + __version__[:-2])
-        app.master.geometry('640x480')
+        app.master.geometry('{}x{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
         app.master.resizable(False, False)
         # Run the main loop of Tcl.
-        app.mainloop()
+        app.master.mainloop()
     
     # Use console.
     elif len(argv) > 1:
@@ -73,13 +174,12 @@ def main(argv):
             dir_path = Path(argv[1])
             found_files = scan_directory(dir_path)
 
-            # Print results to file.
+            # Print to given file.
             if len(argv) > 2:
-                file_path = Path(argv[2])###
+                file_path = Path(argv[2]).with_suffix('.txt')
                 with file_path.open('w') as fhand:
                     print(*found_files, sep='\n', file=fhand)
-            
-            # if no file_path print to console.
+            # Print to console if no file_path entered.
             else:
                 print(*found_files, sep='\n')
 
@@ -88,13 +188,17 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     import sys
 
     main(sys.argv)
 
 
 # TODO:
-# - if file_path doesn't have ".txt" (suffix)
+# - arrange_widgets()
+# from tkinter import messagebox
+# messagebox.showinfo(title='12345', message='About')
+# =messagebox.askquestion("Simple Question", "Do you love Python?")
+
+# - scan_directory()
 # - if posiible do scan recursively
 # - maxdepth
